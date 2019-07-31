d9f3214094c46aa95cb36b1a425305699952c1a0
Schm1tz1 <ro-man@web.de>
2019-07-31 10:13:04 +0200

    initial import

---
+++ src/main/java/com/github/schm1tz1/springKafkaStreamsDemo/SpringKafkaStreamsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.schm1tz1.springKafkaStreamsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaStreamsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaStreamsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/schm1tz1/springKafkaStreamsDemo/SpringKafkaStreamsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.schm1tz1.springKafkaStreamsDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKafkaStreamsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
